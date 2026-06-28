7816609d3c0ab90042d80dd982e7a0c4457f4d17
BionicMango <sagezev@gmail.com>
2026-06-28 13:54:03 +1000

    Updated RPM & Added Timer
    RPM is now rounded to the nearest integer; it is now color-coded.

--- game-1/scripts/tempo.gd
+++ game-1/scripts/tempo.gd
@@ -1,7 +1,16 @@
 extends Label
-@onready var machine = $"../RecordPlayer";
+@export var Machine: StaticBody2D;
 
 var v_rpm: float;
 
+func update_color():
+	if Machine.desired_rpm < Machine.rpm - 150:
+		add_theme_color_override("font_color", Color.ORANGE);
+	elif Machine.desired_rpm > Machine.rpm + 150:
+		add_theme_color_override("font_color", Color.RED);
+	else:
+		add_theme_color_override("font_color", Color.GREEN);
+
 func _process(_delta: float) -> void:
-	text = 'RPM: ' + str(machine.rpm);
+	text = 'RPM: ' + str(int(round(Machine.rpm)));
+	update_color();
